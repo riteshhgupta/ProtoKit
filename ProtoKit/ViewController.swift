7044da05ba850b9b0eec3af4422d624e00645f07
RITESH GUPTA <rit.nsit@gmail.com>
2017-12-05 17:49:29 +0530

    Initial Commit

---
+++ ProtoKit/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ProtoKit
+//
+//  Created by Ritesh Gupta on 05/12/17.
+//  Copyright © 2017 Ritesh Gupta. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view, typically from a nib.
+	}
+
+	override func didReceiveMemoryWarning() {
+		super.didReceiveMemoryWarning()
+		// Dispose of any resources that can be recreated.
+	}
+
+
+}
+
